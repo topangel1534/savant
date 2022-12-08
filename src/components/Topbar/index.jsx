70af8ea4b93c83ee1793e358649c8a9b7018e496
devmachine <rich.swift426@gmail.com>
2022-12-08 13:41:07 -0500

    Add topbar component

---
+++ src/components/Topbar/index.jsx
@@ -0,0 +1,12 @@
+import "./index.scss";
+
+const Topbar = () => {
+  return (
+    <div className="top-bar">
+      <div>Sign Up</div>
+      <div>Log In</div>
+    </div>
+  );
+};
+
+export default Topbar;
---
+++ src/layouts/Header/index.jsx
@@ -0,0 +1,13 @@
+import Topbar from "../../components/Topbar";
+
+import "./index.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Topbar />
+    </div>
+  );
+};
+
+export default Header;
