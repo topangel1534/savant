1a31a339762692ec7d2ff8b3d9870744d16a79fa
munishkmr68 <59211593+munishkmr68@users.noreply.github.com>
2023-01-17 17:19:49 +0530

    Join the waitlist Popup

--- components/Newsletter/index.jsx
+++ components/Newsletter/index.jsx
@@ -1,13 +1,43 @@
-import { Form, Button } from 'react-bootstrap';
+import React, { useState, useRef } from 'react';
+import { Form, Modal, Button } from 'react-bootstrap';
 import form from '../../styles/Form.module.css'
 
 export const Newsletter = (props) => {
-    return ( 
-        <Form className={`${form.getStartedForm} ${form.commonForm}`}>
-            <Form.Group className={`form-group d-md-flex align-items-center gap-3 ${form.formGroup}`} controlId="formContactName">
-                <Form.Control className={`form-control ${form.formControl}`} type="text" placeholder="Enter Your Email" />
-                <Button className={`mt-3 mt-md-0 ${form.mainFrmBtn} ${form.btnRadiusNo}`} variant="primary" type="submit">Join the Waitlist</Button>
-            </Form.Group>
-        </Form> 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <>
+            <Form className={`${form.getStartedForm} ${form.commonForm}`}>
+                <Form.Group className={`form-group d-md-flex align-items-center gap-3 ${form.formGroup}`} controlId="formContactName">
+                    <Form.Control className={`form-control ${form.formControl}`} type="text" placeholder="Enter Your Email" />
+                    <Button className={`mt-3 mt-md-0 ${form.mainFrmBtn} ${form.btnRadiusNo}`} variant="primary" type="button" onClick={handleShow}>Join the Waitlist</Button>
+                </Form.Group>
+            </Form>
+
+            <Modal show={show} onHide={handleClose} className="joinWaitlistModal" centered dialogClassName="joinWaitlistModalDialog">
+                <Modal.Header closeButton>
+                <Modal.Title>Join the Waitlist</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form className={`${form.joinWaitlistForm} ${form.commonForm}`}>
+                        <Form.Group className={`form-group mb-4 ${form.formGroup}`} controlId="formContactName">
+                            <Form.Label className={`${form.formLabel}`}>First Name*</Form.Label>
+                            <Form.Control className={`form-control ${form.formControl}`} type="text" placeholder="Enter Your First Name" />
+                        </Form.Group>
+                        <Form.Group className={`form-group mb-4 ${form.formGroup}`} controlId="formContactName">
+                            <Form.Label className={`${form.formLabel}`}>Last Name*</Form.Label>
+                            <Form.Control className={`form-control ${form.formControl}`} type="text" placeholder="Enter Your Last Name" />
+                        </Form.Group>
+                        <Form.Group className={`form-group mb-4 ${form.formGroup}`} controlId="formContactName">
+                            <Form.Label className={`${form.formLabel}`}>Email*</Form.Label>
+                            <Form.Control className={`form-control ${form.formControl}`} type="text" placeholder="Enter Your Email" />
+                        </Form.Group>
+                        <Button className={`mt-3 mt-md-0 w-100 ${form.mainFrmBtn} ${form.disable} ${form.btnRadiusNo}`} variant="primary" type="button">Send</Button>
+                    </Form>    
+                </Modal.Body>
+            </Modal>
+        </>
     )
 }
