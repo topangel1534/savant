3ba7ec2a5bff7e9a0b18f8556e2971fe2ea5562b
munishkmr68 <59211593+munishkmr68@users.noreply.github.com>
2023-01-18 18:08:23 +0530

    Update waitlist popup

--- components/Newsletter/index.jsx
+++ components/Newsletter/index.jsx
@@ -1,19 +1,38 @@
 import React, { useState, useRef } from 'react';
 import { Form, Modal, Button } from 'react-bootstrap';
 import form from '../../styles/Form.module.css'
+import validator from 'validator'
 
 export const Newsletter = (props) => {
     const [show, setShow] = useState(false);
 
+    const [emailInput, setemailInput] = useState('');
+    const [isWarning, setIsWarning] = useState(false);
+
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleShow = e => {
+        if(validator.isEmail(emailInput)){
+            setShow(true);
+            setIsWarning(false);
+        }else{
+            setShow(false);
+            setIsWarning(true);
+        }
+    }
+
+    const handleTextbox1Change = e => {
+        setemailInput(e.target.value);
+        setIsWarning(false);
+    }
+
     return (
         <>
             <Form className={`${form.getStartedForm} ${form.commonForm}`}>
                 <Form.Group className={`form-group d-md-flex align-items-center gap-3 ${form.formGroup}`} controlId="formContactName">
-                    <Form.Control className={`form-control ${form.formControl}`} type="text" placeholder="Enter Your Email" />
-                    <Button className={`mt-3 mt-md-0 ${form.mainFrmBtn} ${form.btnRadiusNo}`} variant="primary" type="button" onClick={handleShow}>Join the Waitlist</Button>
+                    <Form.Control className={`form-control ${form.formControl}`} type="text" placeholder="Enter Your Email" onChange={handleTextbox1Change} />
+                    <Button className={`mt-3 mt-md-0 ${form.mainFrmBtnEmail} ${form.mainFrmBtn} ${form.btnRadiusNo}`} variant="primary" type="button" onClick={handleShow}>Join the Waitlist</Button>
                 </Form.Group>
+                <spam className={(isWarning) ? form.error : form.hide }>Please enter a valid email</spam>
             </Form>
 
             <Modal show={show} onHide={handleClose} className="joinWaitlistModal" centered dialogClassName="joinWaitlistModalDialog">
@@ -32,9 +51,9 @@ export const Newsletter = (props) => {
                         </Form.Group>
                         <Form.Group className={`form-group mb-4 ${form.formGroup}`} controlId="formContactName">
                             <Form.Label className={`${form.formLabel}`}>Email*</Form.Label>
-                            <Form.Control className={`form-control ${form.formControl}`} type="text" placeholder="Enter Your Email" />
+                            <Form.Control className={`form-control disabled ${form.formControl}`} value={emailInput} type="text" placeholder="Enter Your Email" />
                         </Form.Group>
-                        <Button className={`mt-3 mt-md-0 w-100 ${form.mainFrmBtn} ${form.disable} ${form.btnRadiusNo}`} variant="primary" type="button">Send</Button>
+                        <Button className={`mt-3 mt-md-0 w-100 ${form.mainFrmBtn} ${form.btnRadiusNo}`} variant="primary" type="button">Send</Button>
                     </Form>    
                 </Modal.Body>
             </Modal>
