6f8a3a8327641ef5a3b0a84e0ebed19d92752f0f
Munish <munish.kmr60@gmail.com>
2023-02-04 17:06:56 +0530

    Installed hotjar and ads code

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,32 @@
 // add bootstrap css 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+import Script from 'next/script'
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    hotjar.initialize(3349926, 6)
+  }, [])
+  return <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=AW-11072346824"/>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-11072346824', {
+        page_path: window.location.pathname,
+        });
+      `,
+      }}
+    />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
